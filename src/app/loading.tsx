2eda842019c0a88e0dc33ff948b29bee6457c5db
mateofay2099 <mateofay2099@gmail.com>
2024-07-19 15:02:49 +0200

    add padding to error and loading screens

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,15 @@ import Image from "next/image";
 
 export default function Loading() {
   return (
-    <div style={{ height: "100vh", display: "flex", alignItems: "center", justifyContent: 'center' }}>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: "2rem",
+      }}
+    >
       <Image src="/loadingSpinner.svg" alt="Loading" width={100} height={100} />
     </div>
   );
