430e37eaea9372ef7b522a184b6bf3d63d743930
mateofay2099 <mateofay2099@gmail.com>
2024-07-19 15:02:49 +0200

    add padding to error and loading screens

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -4,7 +4,14 @@ import { Error as ErrorComponent } from "@components/common/Error";
 export default function Error() {
   return (
     <div style={{ height: "100vh", position: "relative" }}>
-      <div style={{ position: "absolute", top: "15%", width: "100%" }}>
+      <div
+        style={{
+          position: "absolute",
+          top: "15%",
+          width: "100%",
+          padding: "2rem",
+        }}
+      >
         <ErrorComponent />
       </div>
     </div>
